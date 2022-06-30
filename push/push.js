7a4c7047dde2e4092e3b0a975f14f260bd20eb93
YutoUrushima <yuto259233@gmail.com>
2022-06-30 17:15:06 +0900

    Add axios to use LINE Messaging API

--- push/push.js
+++ push/push.js
@@ -4,7 +4,35 @@
  * @param {!Object} event Event payload.
  * @param {!Object} context Metadata for the event.
  */
+
+const axios = require('axios');
+
 exports.push = (event, context) => {
 	const message = event.data ? Buffer.from(event.data, 'base64').toString() : 'Hello, World';
-	console.log(message);
+	push(message);
 };
+
+async function push(message) {
+	await axios
+		.post('https://api.line.me/v2/bot/message/push', {
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${process.env.CHANNEL_ACCESS_TOKEN}`,
+			},
+			data: {
+				to: process.env.TO,
+				messages: [
+					{
+						'type': 'text',
+						'text': message,
+					},
+				],
+			},
+		})
+		.then((response) => {
+			console.log(response);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
