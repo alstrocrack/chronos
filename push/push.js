56c7f70a61763ae90d657e21501348d9f6538c08
YutoUrushima <yuto259233@gmail.com>
2022-06-30 15:08:51 +0900

    Fix directories

---
+++ push/push.js
@@ -0,0 +1,10 @@
+/**
+ * Triggered from a message on a Cloud Pub/Sub topic.
+ *
+ * @param {!Object} event Event payload.
+ * @param {!Object} context Metadata for the event.
+ */
+exports.push = (event, context) => {
+	const message = event.data ? Buffer.from(event.data, 'base64').toString() : 'Hello, World';
+	console.log(message);
+};
