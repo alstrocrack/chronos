b93b5f26c8eb11f9b4d61dfbf7a8ee22963c3be8
YutoUrushima <yuto259233@gmail.com>
2022-07-02 15:21:13 +0900

    Fix logic

--- push/push.js
+++ push/push.js
@@ -7,40 +7,32 @@
 
 const axios = require('axios');
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
-const CHANNEL_ACCESS_TOKEN = 'projects/199194440168/secrets/CHANNEL_ACCESS_TOKEN/versions/latest';
-const TO = 'projects/199194440168/secrets/TO/versions/latest';
+const pathTochannelAccessToken = 'projects/199194440168/secrets/CHANNEL_ACCESS_TOKEN/versions/latest';
+const pathToAdminTo = 'projects/199194440168/secrets/ADMIN_TO/versions/latest';
 
 const client = new SecretManagerServiceClient();
 
 exports.push = async (event, context) => {
-	const cat = await accessSecretVersion(CHANNEL_ACCESS_TOKEN);
-	const to = await accessSecretVersion(TO);
-	console.error(`cat: ${cat}`);
-	console.error(`to: ${to}`);
-	push(cat, to, 'hello');
+	const channelAccessToken = await accessSecretVersion(pathTochannelAccessToken);
+	const adminTo = await accessSecretVersion(pathToAdminTo);
+	push(channelAccessToken, adminTo, 'hello');
 };
 
-async function accessSecretVersion(key) {
+async function accessSecretVersion(secretKey) {
 	const [version] = await client.accessSecretVersion({
-		name: key,
+		name: secretKey,
 	});
-
-	// Extract the payload as a string.
 	const payload = version.payload.data.toString();
-
-	// WARNING: Do not print the secret in a production environment - this
-	// snippet is showing how to access the secret material.
-	console.info(`Payload: ${payload}`);
 	return payload;
 }
 
-async function push(cat, to, message = null) {
+async function push(channelAccessToken, to, message = null) {
 	await axios({
 		method: 'post',
 		url: 'https://api.line.me/v2/bot/message/push',
 		headers: {
 			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${cat}`,
+			'Authorization': `Bearer ${channelAccessToken}`,
 		},
 		data: {
 			to: to,
@@ -57,9 +49,5 @@ async function push(cat, to, message = null) {
 		})
 		.catch((error) => {
 			console.log(error);
-		})
-		.finally(() => {
-			// console.error(`cat: ${accessSecretVersion(CHANNEL_ACCESS_TOKEN)}`);
-			// console.error(`to: ${accessSecretVersion(TO)}`);
 		});
 }
