0c50434daedc660d76c702cda0a159f3b9463707
YutoUrushima <yuto259233@gmail.com>
2022-07-02 12:49:10 +0900

    Fix push message & Add axios google-cloud/secret-manager

--- main/main.js
+++ main/main.js
@@ -4,7 +4,33 @@
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
+const axios = require('axios');
+
 exports.main = (req, res) => {
 	let message = req.query.message || req.body.message || 'Hello World!';
 	res.status(200).send(message);
+	reply(req.body.events[0].replyToken);
 };
+
+// async function reply(replyToken, message = null) {
+// 	await axios
+// 		.post('https://api.line.me/v2/bot/message/reply', {
+// 			headers: {
+// 				'Content-Type': 'application/json',
+// 				'Authorization': `Bearer ${process.env.CHANNEL_ACCESS_TOKEN}`,
+// 			},
+// 			data: {
+// 				replyToken: replyToken,
+// 				messages: [
+// 					{
+// 						'type': 'text',
+// 						'text': message,
+// 					},
+// 				],
+// 			},
+// 		})
+// 		.then((response) => {
+// 			console.log(response);
+// 		})
+// 		.catch((error) => {});
+// }
--- push/push.js
+++ push/push.js
@@ -6,29 +6,42 @@
  */
 
 const axios = require('axios');
+// const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+const CHANNEL_ACCESS_TOKEN = 'projects/199194440168/secrets/CHANNEL_ACCESS_TOKEN';
+const TO = 'projects/199194440168/secrets/TO';
+
+// const client = new SecretManagerServiceClient();
 
 exports.push = (event, context) => {
-	const message = event.data ? Buffer.from(event.data, 'base64').toString() : 'Hello, World';
-	push(message);
+	push('hello');
 };
 
-async function push(message) {
-	await axios
-		.post('https://api.line.me/v2/bot/message/push', {
-			headers: {
-				'Content-Type': 'application/json',
-				'Authorization': `Bearer ${process.env.CHANNEL_ACCESS_TOKEN}`,
-			},
-			data: {
-				to: process.env.TO,
-				messages: [
-					{
-						'type': 'text',
-						'text': message,
-					},
-				],
-			},
-		})
+// async function getSecret() {
+// 	const [secret] = await client.getSecret({
+// 		to: TO,
+// 		channelAccessToken: CHANNEL_ACCESS_TOKEN,
+// 	});
+// 	return [secret];
+// }
+
+async function push(message = null) {
+	await axios({
+		method: 'post',
+		url: 'https://api.line.me/v2/bot/message/push',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${process.env.CHANNEL_ACCESS_TOKEN}`,
+		},
+		data: {
+			to: process.env.TO,
+			messages: [
+				{
+					'type': 'text',
+					'text': message,
+				},
+			],
+		},
+	})
 		.then((response) => {
 			console.log(response);
 		})
