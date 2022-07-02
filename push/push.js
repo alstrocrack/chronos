ae092a1875ac835cf6bea3cc2339117b2250d383
YutoUrushima <yuto259233@gmail.com>
2022-07-02 14:54:55 +0900

    Get secrets

--- push/push.js
+++ push/push.js
@@ -6,34 +6,44 @@
  */
 
 const axios = require('axios');
-// const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
-const CHANNEL_ACCESS_TOKEN = 'projects/199194440168/secrets/CHANNEL_ACCESS_TOKEN';
-const TO = 'projects/199194440168/secrets/TO';
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+const CHANNEL_ACCESS_TOKEN = 'projects/199194440168/secrets/CHANNEL_ACCESS_TOKEN/versions/latest';
+const TO = 'projects/199194440168/secrets/TO/versions/latest';
 
-// const client = new SecretManagerServiceClient();
+const client = new SecretManagerServiceClient();
 
-exports.push = (event, context) => {
-	push('hello');
+exports.push = async (event, context) => {
+	const cat = await accessSecretVersion(CHANNEL_ACCESS_TOKEN);
+	const to = await accessSecretVersion(TO);
+	console.error(`cat: ${cat}`);
+	console.error(`to: ${to}`);
+	push(cat, to, 'hello');
 };
 
-// async function getSecret() {
-// 	const [secret] = await client.getSecret({
-// 		to: TO,
-// 		channelAccessToken: CHANNEL_ACCESS_TOKEN,
-// 	});
-// 	return [secret];
-// }
+async function accessSecretVersion(key) {
+	const [version] = await client.accessSecretVersion({
+		name: key,
+	});
 
-async function push(message = null) {
+	// Extract the payload as a string.
+	const payload = version.payload.data.toString();
+
+	// WARNING: Do not print the secret in a production environment - this
+	// snippet is showing how to access the secret material.
+	console.info(`Payload: ${payload}`);
+	return payload;
+}
+
+async function push(cat, to, message = null) {
 	await axios({
 		method: 'post',
 		url: 'https://api.line.me/v2/bot/message/push',
 		headers: {
 			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${process.env.CHANNEL_ACCESS_TOKEN}`,
+			'Authorization': `Bearer ${cat}`,
 		},
 		data: {
-			to: process.env.TO,
+			to: to,
 			messages: [
 				{
 					'type': 'text',
@@ -47,5 +57,9 @@ async function push(message = null) {
 		})
 		.catch((error) => {
 			console.log(error);
+		})
+		.finally(() => {
+			// console.error(`cat: ${accessSecretVersion(CHANNEL_ACCESS_TOKEN)}`);
+			// console.error(`to: ${accessSecretVersion(TO)}`);
 		});
 }
