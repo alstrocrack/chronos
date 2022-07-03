cd03d513c764b49253771264998e2e47a685badd
YutoUrushima <yuto259233@gmail.com>
2022-07-03 16:53:30 +0900

    Add connection MySQL

--- main/main.js
+++ main/main.js
@@ -5,32 +5,45 @@
  * @param {!express:Response} res HTTP response context.
  */
 const axios = require('axios');
+const mysql = require('mysql');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+const pathToDBUser = 'projects/199194440168/secrets/DB_USER/versions/latest';
+const pathToDBPass = 'projects/199194440168/secrets/DB_PASS/versions/latest';
+const pathToDBHost = 'projects/199194440168/secrets/DB_HOST/versions/latest';
+const pathToDBPort = 'projects/199194440168/secrets/DB_PORT/versions/latest';
 
-exports.main = (req, res) => {
-	let message = req.query.message || req.body.message || 'Hello World!';
-	res.status(200).send(message);
-	reply(req.body.events[0].replyToken);
+const client = new SecretManagerServiceClient();
+
+exports.main = async (req, res) => {
+	const [dbUser, dbPass, dbHost, dbPort] = await Promise.all([
+		accessSecretVersion(pathToDBUser),
+		accessSecretVersion(pathToDBPass),
+		accessSecretVersion(pathToDBHost),
+		accessSecretVersion(pathToDBPort),
+	]);
+
+	const connection = mysql.createConnection({
+		host: dbHost,
+		user: dbUser,
+		password: dbPass,
+		port: dbPort,
+	});
+
+	connection.connect((error) => {
+		if (error) {
+			console.log(`Connection Error: ${error}`);
+		}
+	});
+	connection.query(`SHOW DATABASES`, (err, results) => {
+		err ? console.log(err) : console.log(JSON.stringify({ results }));
+	});
+	res.send('OK');
 };
 
-// async function reply(replyToken, message = null) {
-// 	await axios
-// 		.post('https://api.line.me/v2/bot/message/reply', {
-// 			headers: {
-// 				'Content-Type': 'application/json',
-// 				'Authorization': `Bearer ${process.env.CHANNEL_ACCESS_TOKEN}`,
-// 			},
-// 			data: {
-// 				replyToken: replyToken,
-// 				messages: [
-// 					{
-// 						'type': 'text',
-// 						'text': message,
-// 					},
-// 				],
-// 			},
-// 		})
-// 		.then((response) => {
-// 			console.log(response);
-// 		})
-// 		.catch((error) => {});
-// }
+async function accessSecretVersion(secretKey) {
+	const [version] = await client.accessSecretVersion({
+		name: secretKey,
+	});
+	const payload = version.payload.data.toString();
+	return payload;
+}
