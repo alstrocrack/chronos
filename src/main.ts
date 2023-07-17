7e2051f3483005260e2e25a6a4baf47bf56f8f35
YutoUrushima <yuto259233@gmail.com>
2023-07-17 18:00:08 +0900

    Add logic to follow event

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,51 @@
 import * as line from '@line/bot-sdk';
+import { WebhookRequestBody, FollowEvent, Message, Client } from '@line/bot-sdk';
+import * as mysql from 'mysql2';
+import { ConnectionOptions, ResultSetHeader, Connection } from 'mysql2';
 
-const config = {
+const lineConfig = {
 	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN!,
 	channelSecret: process.env.CHANNEL_SECRET!,
 };
 
-export const handler = async (event: any, context: any) => {
-	const client = new line.Client(config);
+const dbConfig: ConnectionOptions = {
+	host: process.env.DB_HOST!,
+	user: process.env.DB_USER!,
+	password: process.env.DB_PASSWORD!,
+	database: process.env.DB_NAME!,
+	port: Number(process.env.DB_PORT!),
+};
 
-	const message: line.Message = {
+export const handler = async (event: WebhookRequestBody, context: any) => {
+	const lineClient = new line.Client(lineConfig);
+	const db = mysql.createConnection(dbConfig);
+
+	const events: Array<any> = event.events;
+
+	events.forEach((event) => {
+		const eventType: string = event.type;
+
+		switch (eventType) {
+			case 'follow':
+				registerEvent(lineClient, event, db);
+		}
+	});
+};
+
+const registerEvent = async (client: Client, event: FollowEvent, db: Connection) => {
+	const userInsertQuery = `
+		INSERT INTO user_accounts (id, created_at, updated_at) VALUES (?, Now(), Now());
+	`;
+	if (!event.source.userId) {
+		throw new Error('FOLLOW: Invalid user token');
+	}
+	const userId: string = event.source.userId;
+	db.query<ResultSetHeader>(userInsertQuery, userId, (err, rows) => {
+		console.log(err, rows);
+	});
+	const message: Message = {
 		type: 'text',
-		text: 'hello',
+		text: 'Birthday Reminderを登録ありがとうございます！',
 	};
-
-	await client.pushMessage(process.env.MY_ACCOUNT!, message);
+	await client.replyMessage(event.replyToken, message);
 };
