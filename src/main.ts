15db7652d59b9ac99df92f2e661800b4536ed50c
YutoUrushima <yuto259233@gmail.com>
2023-07-17 15:38:06 +0900

    Success to push message with SDK

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
+import * as line from '@line/bot-sdk';
+
+const config = {
+	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN!,
+	channelSecret: process.env.CHANNEL_SECRET!,
+};
+
 export const handler = async (event: any, context: any) => {
-	console.log(event);
+	const client = new line.Client(config);
+
+	const message: line.Message = {
+		type: 'text',
+		text: 'hello',
+	};
+
+	await client.pushMessage(process.env.MY_ACCOUNT!, message);
 };
